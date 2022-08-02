14758466b658ce323e0c35d3c572116b017c50c0
jackhall0607 <jackhall1996@gmail.com>
2022-08-02 23:09:37 +0100

    Identity line option in cor.plot

--- R/cor.plot.r
+++ R/cor.plot.r
@@ -12,16 +12,23 @@ cor.plot <- function(x, ...){
 #' Correlation Plot for kd_model
 #'
 #' @param x a kd_model object
+#' @param identity_line include a line of x=y, default TRUE
 #' @param ... additional parameters
 #'
 #' @return a plot
 #' @export
-cor.plot.kd_model <- function(x, ...){
-  cbind(prediction = predict(x$predict_model, x$data$inputs, verbose = 0)[[4]],
+cor.plot.kd_model <- function(x, identity_line = TRUE, ...){
+  plot <- cbind(prediction = predict(x$predict_model, x$data$inputs, verbose = 0)[[4]],
         actual = x$data$response$rate) %>%
     as.data.frame %>%
     ggplot2::ggplot() +
-    ggplot2::geom_point(ggplot2::aes(x = actual, y = prediction))
+    ggplot2::geom_point(ggplot2::aes(x = actual, y = prediction), ...)
+
+  if(identity_line){
+    plot <- plot +
+      ggplot2::geom_function(fun = function(x) x)
+  }
+  plot
 }
 
 #' Correlation Plot for kd_model_htest
